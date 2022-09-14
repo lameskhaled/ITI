5ad556e85977798c333b160482afe3dc063d9365
Lamees Khaled Abdelrazek <91064862+lameskhaled@users.noreply.github.com>
2022-09-14 23:51:09 +0200

    Add files via upload

---
+++ MongoDB/Task 3/task 3.js
@@ -0,0 +1,42 @@
+use inventory
+
+//a
+departments = db.department.find({},{department_description: 1}).toArray();
+db.employee.find({},{full_name: 1}).forEach(employee=>{
+    deps = departments.find(dep=>{
+        dep._id == employee.department_id;
+        print(`${employee.full_name} : ${dep.department_description}`)
+        })
+    });
+    
+//b
+db.employee.find({position_title: "VP Country Manager"}, {full_name: 1, salary: 1});
+
+//c
+let regions = db.region.find().toArray();
+db.customer.find({}, {fullname: 1, "address.customer_region_id": 1}).forEach(customer => {
+    let region = regions.find(reg => {
+        reg.region_id == customer.address.customer_region_id;
+        print(`${customer.fullname} :${reg.sales_region} `);})
+});
+
+//d
+db.product.find({brand_name: "Washington"}, {product_name:1});
+
+//Aggregate
+//a
+db.product.aggregate([
+{$project:{_id:0, brand_name:1}},
+{$group: {"_id": "$brand_name", count: {$sum: 1}}}
+])
+
+//b
+db.product.aggregate([
+{$match: {brand_name: {$in: ["Blue Label","King","Washington"]}}},
+{$sort: {brand_name: 1, product_name: 1}},
+{$project: {_id: 0, Brand_Name: "$brand_name", Product_Name: "$product_name"}},
+{$group: {_id: {Brand_Name: "$Brand_Name", Product_Name: "$Product_Name"}}}
+]);
+
+    
+    
---
+++ MongoDB/Task2.js
@@ -0,0 +1,64 @@
+//a- Display all documents in instructors collection
+db.instructors.find({});
+
+//b- Display all instructors with salaries greater than 4000 (only show firstName and salary)
+db.instructors.find({salary:{$gt:4000}},{_id:0,firstName:1,salary:1});
+
+//c- Display all instructors with ages less than or equal 25
+db.instructors.find({age:{$lte:25}},{_id:0,firstName:1,age:1});
+
+//d- Display all instructors with city mansoura and sreet number 10 or 14 only display (firstname,address,salary).
+db.instructors.find({$and:[{"fullAddress.city": "Ismailia"},{$or:[{"fullAddress.street": 10}, {"fullAddress.street": 14}]}]},{_id: 0, firstName: 1, fullAddress: 1, salary: 1});
+
+//e- Display all instructors who have js and jquery in their courses (both of them not one of them).
+db.instructors.find({courses:{$all:["js","jquery"]}});
+
+//f- Display number of courses for each instructor and display first name with number of courses.
+db.instructors.find({courses:{$exists:true}}).forEach(instructor=>{
+print(`${instructor.firstName} , courses :${instructor.courses.length}`);})
+
+//g- Write mongodb query to get all instructors that have firstName and 
+//lastName properties , sort them by firstName ascending then by 
+//lastName descending and finally display their data FullName and age 
+let instdData2 = [];
+db.instructors.find({firstName:{$exists:true},lastName:{$exists:true}}).sort({firstName:1,lastName:-1}).forEach(instructor=>{
+instdData2.push({"full name":instructor.firstName+" "+instructor.lastName,"age":instructor.age});
+print(`full name : ${instructor.firstName} ${instructor.lastName}, age :${instructor.age}`);})
+
+//g- BONUS: create new collection with step g values data and name it instructorsData
+db.instructorsData.insert(instdData2);
+db.instructorsData.find({},{_id:0});
+
+//h- Find all instructors that have fullName that contains “mohammed”.
+db.instructors.find({$or:[{firstName:"mohammed"},{lastName:"mohammed"}]});
+
+//i- Delete instructor with first name “ebtesam” and has only 5 courses in courses array
+db.instructors.remove({firstName:"ebtesam",courses:{$size:5}});
+
+//j- Add active property to all instructors and set its value to true.
+db.instructors.updateMany({},{$set:{active:true}});
+db.instructors.find();
+
+//k- Change “EF” course to “jquery” for “mazen mohammed” instructor (without knowing EF Index)
+db.instructors.updateOne({ firstName: "mazen", lastName: "mohammed", courses: "EF" },{$set: {"courses.$": "jquery"}});
+db.instructors.find();
+
+//l- Add jquery course for “noha hesham”.
+db.instructors.updateOne({ firstName: "noha", lastName: "hesham"},{$addToSet: {"courses": "jquery"}});
+db.instructors.find();
+
+//m- Remove courses property for “ahmed mohammed ” instructor
+db.instructors.updateOne({firstName: "ahmed", lastName: "mohammed"},{$unset:{courses:""}});
+db.instructors.find();
+
+//n - Decrease salary by 500 for all instructors that has only 3 courses in their list ($inc)
+db.instructors.updateMany({courses:{$size:3}},{$inc:{salary:-500}});
+db.instructors.find();
+
+//o - Rename address field for all instructors to fullAddress.
+db.instructors.updateMany({},{$rename:{address:"fullAddress"}});
+db.instructors.find();
+
+//p - Change street number for noha hesham to 20
+db.instructors.update({firstName: "noha", lastName: "hesham"},{$set:{"fullAddress.street":20}});
+db.instructors.find();
